1bc5f7d85796b955e3938397447a3bd1d248e518
manserman <mansourh923@gmail.com>
2023-06-14 17:30:24 +0200

    moi

--- src/main/java/GrilleImpl.java
+++ src/main/java/GrilleImpl.java
@@ -1,15 +1,33 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Set;
 
 public class GrilleImpl implements Grille {
+    private final int dimension;
     private int lignes=9;
     private int colonnes=9;
     private ElementDeGrille[] elementsInitiales;
+    private final ElementDeGrille[] alphabet;
+    private char position[][];
+    private ElementDeGrille position1[][];
+ public GrilleImpl(ElementDeGrille[] elementDeGrilles, ElementDeGrille[][] grilleTab) {
+        dimension=elementDeGrilles.length;
+        alphabet=elementDeGrilles;
+        position1=grilleTab;
+
+    }
     public GrilleImpl(int x,int y){
       this.lignes=x;
       this.colonnes=y;
     }
     public GrilleImpl(ElementDeGrille[] elements){
+        position=new char[lignes][colonnes];
      this.elementsInitiales=elements;
+     init();
+    }
+    private void init(){
+
     }
      public GrilleImpl(int x,int y,ElementDeGrille[] elements){
       this.lignes=x;
@@ -59,5 +77,16 @@ public class GrilleImpl implements Grille {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'isValeurInitiale'");
     }
+     private void placerElementsAleatoirement() {
+
+        for (int i = 0; elementsInitiales; i++) {
+            
+              
+                   
+                }
+            }
+        
+    }
+
     
 }
